0dd1102bef8ce68ab87f3497bd71d9b8a4d455b9
parinaz fa <parinaz.fa@gmail.com>
2020-03-29 00:58:37 -0400

    my_first_python_package

---
+++ README.md
@@ -0,0 +1,8 @@
+# GBI-distribution package
+
+This package plot Gaussian and Binomial distributions and can add them
+
+# files 
+
+General distiribution super class
+Gaussian and Binomial are inherent
---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 28 23:59:00 2020
+
+@author: parinazfarajiparvar
+"""
+
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
